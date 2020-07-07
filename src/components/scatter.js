c4915faab6a53874275c8a57f6f1fea1034c471a
AlanPalomino <55060860+AlanPalomino@users.noreply.github.com>
2020-07-07 02:46:49 -0400

    update

--- src/components/scatter.js
+++ src/components/scatter.js
@@ -14,8 +14,8 @@ const data = {
       pointHoverRadius: 5,
       pointHoverBackgroundColor: 'rgba(75,192,192,1)',
       pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
+      pointHoverBorderWidth: 5,
+      pointRadius: 4,
       pointHitRadius: 10,
       data: [
         { x: 65, y: 75 },
