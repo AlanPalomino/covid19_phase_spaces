1e9181a2e301ef295b816c98fdebbf42d0bc4cc8
AlanPalomino <55060860+AlanPalomino@users.noreply.github.com>
2020-07-06 23:57:47 -0400

    update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DropdownMSS from './components/DropdownMenu';
+import ScatterExample from './components/scatter.js';
+import { covid_data_ww } from "./covid_data_ww";
 
 
-function App() {
+export default class App extends React.Component {
+//function App() {
+  render(){
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
         <h1>Diagramas de Estados-Fase de Covid19 a Nivel Mundial </h1>
         <img src={logo} className="App-logo" alt="logo" width="250" height="270" />
@@ -14,11 +18,14 @@ function App() {
       <h3>Seleccione los paises que desee visualizar. </h3>
       <div>
       <DropdownMSS />
+      <hr />
       </div>
+      
+				<ScatterExample data={covid_data_ww}/> 
+			<hr />
 
-        
     </div>
   );
+  }
 }
-
-export default App;
+//export default App;
