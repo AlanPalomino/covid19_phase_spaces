b80111d5bb367b1ac0ca0930fbf5ac70ff675653
AlanPalomino <55060860+AlanPalomino@users.noreply.github.com>
2020-07-10 17:09:06 -0400

    Update Ploter.js

--- src/components/Ploter.js
+++ src/components/Ploter.js
@@ -1,88 +1,30 @@
 import React, {Component} from 'react';
-import { Scatter } from 'react-chartjs-2';
-import { Dropdown } from 'semantic-ui-react';
-import {covid_data_ww} from '../covid_data_ww.js';
+import {Chart} from 'react-chartjs-2';
 
 
-/*console.log(Object.keys(covid_data_ww))*/
-const countryOptions = Object.keys(covid_data_ww).map( (country) => ({
-  key: country,
-  text: country,
-  value: country,
-}))
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx,{
+    type: 'line',
+    data:{
+        labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+        datasets:[{
+            label: 'Manzanas',
+            data:[12, 19, 3, 17, 6, 3, 7],
+            backgroundColor:'rgba(153,255,51,0.6)'
+        },{
+            label:'Naranjas',
+            data:[2, 29, 5, 5, 2, 3, 10],
+            backgroundColor:"rgba(255,153,0,0.6)"
+        }]
+    },
+});
 
-const datalabel = Object.keys(covid_data_ww).map( (country) => ({
-    key: country,
-    text: country,
-    value: country,
-  }))
-  
-  const dataval = Object.values(covid_data_ww).map( row => ({
-    "x": row["Tasa_Incid"], "y": row["r(t)_casos"],
-  }))
-
-  function addData(chart, label, data) {
-    chart.data.labels.push(label);
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.push(data);
-    });
-    chart.update();
-}
-
-const handleChange = (e, { value }) => {
-    console.log(value);
-    forEach((value) => {
-        addData(Scatter,label=country,data= Object.values(covid_data_ww).map( row => ({
-            "x": row["Tasa_Incid"], "y": row["r(t)_casos"],
-          })))
-  });
-var DropdownMSS = () => (
-  <Dropdown
-    placeholder='Seleccione los paises'
-    fluid
-    multiple
-    search
-    selection
-    onChange={handleChange.bind(this)}
-    options={countryOptions}
-  />
-)
-
-const data_config = {
-    labels: ['Scatter'],
-    datasets: [
-      {
-        label: datalabel[0],
-        fill: false,
-        showLine: true,  
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 5,
-        pointRadius: 4,
-        pointHitRadius: 10,
-        data:  dataval
-      }
-    ],
-    options: {
-        scales:{
-            xAxes: [{type: 'logarithmic'}],
-            yAxes: [{type: 'logarithmic'}]
-        }
-    }
-  };
-  
-  export  class ScatterPlot extends Component {
+export default class Plot extends Component {
     render() {
       return (
         <div>
           <h2>Espacio-Fase de Incidencias</h2>
-          <DropdownMSS/>
-          <Scatter ref="chart" data={data_config} />
+          <myChart />
         </div>
       );
     }
