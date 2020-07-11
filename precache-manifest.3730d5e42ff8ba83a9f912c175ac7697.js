c7185c0d976f8fc8dac6e89262329d139c10c042
AlanPalomino <zappataville@gmail.com>
2020-07-10 20:42:41 -0500

    Updates

---
+++ precache-manifest.3730d5e42ff8ba83a9f912c175ac7697.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d17d2d747508dd24f9ee3cbdcf628cbf",
+    "url": "/covid19_phase_spaces/index.html"
+  },
+  {
+    "revision": "8a5fb97225a898ab43c7",
+    "url": "/covid19_phase_spaces/static/css/main.9a9afc6c.chunk.css"
+  },
+  {
+    "revision": "9025d891101db5515f98",
+    "url": "/covid19_phase_spaces/static/js/2.ae394970.chunk.js"
+  },
+  {
+    "revision": "37513ab48137d96fc6ba56ac78d364d8",
+    "url": "/covid19_phase_spaces/static/js/2.ae394970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5fb97225a898ab43c7",
+    "url": "/covid19_phase_spaces/static/js/main.291dcc40.chunk.js"
+  },
+  {
+    "revision": "7a8796f774f8c33bfd5a",
+    "url": "/covid19_phase_spaces/static/js/runtime-main.93d8e36c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19_phase_spaces/static/media/logo.5d5d9eef.svg"
+  }
+]);
